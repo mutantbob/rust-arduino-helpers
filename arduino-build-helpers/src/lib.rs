dce53bc1855185d708c04db5cb5d020857e33505
Robert Forsman <git@thoth.purplefrog.com>
2022-01-17 16:38:11 -0500

    add some really sloppy examples for .rig_arduino*()

--- arduino-build-helpers/src/lib.rs
+++ arduino-build-helpers/src/lib.rs
@@ -1,6 +1,28 @@
 use cc::Build;
 
 pub trait ArduinoBuilder {
+    /// You would use this like:
+    /// ```
+    /// let anp_dir = format!("{}/vendor/Adafruit_NeoPixel", env!("HOME"));
+    ///
+    /// let mut builder = cc::Build::new();
+    /// builder
+    ///     .include(anp_dir)
+    ///     .rig_arduino(true)
+    ///     .cpp(true)
+    ///     .compiler("avr-g++");
+    ///
+    /// builder.file("src-cpp/neopixel.cpp");
+    ///
+    /// println!("cargo:rustc-link-lib=static=neopixel");
+    /// builder.compile("libneopixel.a");
+    ///```
+    ///
+    /// where `src-cpp/neopixel.cpp`` is
+    /// ```c++
+    /// #include <Arduino.h>
+    /// #include "Adafruit_NeoPixel.cpp"
+    /// ```
     fn rig_arduino(&mut self, c_plus_plus: bool) -> &mut Self;
 }
 
@@ -29,6 +51,29 @@ impl ArduinoBuilder for Build {
 }
 
 pub trait ArduinoBindgen {
+    /// You would use this like
+    /// ```
+    ///
+    /// fn generate_bindings_rs() {
+    ///     let arduino_h = "/usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h";
+    ///     println!("cargo:rerun-if-changed={}", arduino_h);
+    ///     let bindings = bindgen::Builder::default()
+    ///         .header(arduino_h)
+    ///         .rig_arduino_uno()
+    ///         .clang_args(&["-x", "c++"])
+    ///         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+    ///         .ctypes_prefix("crate::workaround_cty") // the cty crate won't compile
+    ///         .generate()
+    ///         .expect("Unable to generate bindings");
+    ///
+    ///     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    ///     let bindings_out_file = out_path.join("bindings.rs");
+    ///     bindings
+    ///         .write_to_file(bindings_out_file)
+    ///         .expect("Couldn't write bindings!");
+    /// }
+    ///
+    /// ```
     fn rig_arduino_uno(self) -> Self;
 }
 
