7937f57352555d6c81e3fdec2e94abbea5e706c8
Robert Forsman <git@thoth.purplefrog.com>
2022-01-17 15:30:23 -0500

    add a #[global_allocator] that wraps malloc & friends

--- rust-arduino-runtime/src/lib.rs
+++ rust-arduino-runtime/src/lib.rs
@@ -1,8 +1,17 @@
 #![no_std]
+#![feature(alloc_error_handler)] // at the top of the file
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::convert::TryInto;
+use workaround_cty::c_void;
 
 mod raw;
 pub mod workaround_cty;
 
+/// This is the magical init() function called by the stock Arduino main() function.
+/// I have not yet figured out what exactly it does,
+/// but I have found that at least one library (Adafruit_NeoPixel) will not work
+/// without some subset of the initializations it performs.
 pub fn arduino_main_init() {
     unsafe {
         raw::init(); // implemented in /usr/share/arduino/hardware/arduino/avr/cores/arduino/wiring.c
@@ -14,6 +23,26 @@ pub fn arduino_serial_event_run() {
         raw::serialEventRun();
     }
 }
+
+/// maybe this works?
+struct ArduinoAlloc;
+
+unsafe impl GlobalAlloc for ArduinoAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        raw::malloc(layout.size().try_into().unwrap()) as *mut u8
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        raw::free(ptr as *mut c_void)
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, new_size: usize) -> *mut u8 {
+        raw::realloc(ptr as *mut c_void, new_size.try_into().unwrap()) as *mut u8
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: ArduinoAlloc = ArduinoAlloc;
 
 #[cfg(test)]
 mod tests {
